3d0936c08d54df53308db6d4c9f17305538c6e04
Scott McCreary <scottmc2@gmail.com>
2009-11-26 12:37:43 +0000

    Fixed missing newline

--- installhaikuporter.sh
+++ installhaikuporter.sh
@@ -12,7 +12,8 @@ if [ $button -eq 0 ] ; then
   		cp haikuporter $(finddir B_COMMON_BIN_DIRECTORY)/haikuporter > /dev/null
 		echo "# HaikuPorts configuration" > haikuports.conf
 		echo "" >> haikuports.conf
-        echo "PACKAGES_PATH="\"$(finddir B_COMMON_DEVELOP_DIRECTORY)/haikuports\" >> haikuports.conf  		cp haikuports.conf $(finddir B_COMMON_ETC_DIRECTORY)/haikuports.conf > /dev/null
+        echo "PACKAGES_PATH="\"$(finddir B_COMMON_DEVELOP_DIRECTORY)/haikuports\" >> haikuports.conf
+        cp haikuports.conf $(finddir B_COMMON_ETC_DIRECTORY)/haikuports.conf > /dev/null
   		alert "haikuporter is ready." > /dev/null
 else
 		alert --stop "haikuporter is not installed." > /dev/null
