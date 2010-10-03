43b2989cc377720e23530fb4922242d910b72896
Scott McCreary <scottmc2@gmail.com>
2010-10-03 17:12:54 +0000

    Added haikuporter -g so that the install scripts will refresh the ports tree.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ $button -eq 0 ] ; then
 		echo "" >> haikuports.conf
 		echo "PACKAGES_PATH="\"$(finddir B_COMMON_DEVELOP_DIRECTORY)/haikuports\" >> haikuports.conf
   		cp haikuports.conf $(finddir B_COMMON_ETC_DIRECTORY)/haikuports.conf > /dev/null
+  		haikuporter -g
   		alert "haikuporter is ready." > /dev/null
 else
 		alert --stop "haikuporter is not installed." > /dev/null
