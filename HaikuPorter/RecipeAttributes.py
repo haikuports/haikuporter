558fbdb04ff6cb063a4afba7f15b6f1bd0343af2
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-10-27 15:55:45 +0100

    Second attempt to fix whitespace. Options are different on OS X.

--- HaikuPorter/RecipeAttributes.py
+++ HaikuPorter/RecipeAttributes.py
@@ -14,7 +14,7 @@
 
 from HaikuPorter.Package import PackageType
 from HaikuPorter.RecipeTypes import (Architectures, Extendable, LinesOfText,
-                                     Phase, ProvidesList, RequiresList, YesNo)
+									 Phase, ProvidesList, RequiresList, YesNo)
 import copy
 import types
 
@@ -22,246 +22,246 @@ import types
 # -- recipe keys and their attributes -----------------------------------------
 
 def getRecipeFormatVersion():
-    """Returns the current version of the recipe format"""
-    return 1
+	"""Returns the current version of the recipe format"""
+	return 1
 
 recipeAttributes = {
-    # non-extendable and non-indexable, i.e. per-port attributes
-    'BUILD_PACKAGE_ACTIVATION_PHASE': {
-        'type': Phase,
-        'required': False,
-        'default': Phase('BUILD'),
-        'extendable': Extendable.NO,
-        'indexable': False,
-    },
-    'DISABLE_SOURCE_PACKAGE': {
-        'type': YesNo,
-        'required': False,
-        'default': False,
-        'extendable': Extendable.NO,
-        'indexable': False,
-    },
-    'HOMEPAGE': {
-        'type': types.ListType,
-        'required': True,
-        'default': None,
-        'extendable': Extendable.NO,
-        'indexable': False,
-    },
-    'MESSAGE': {
-        'type': types.StringType,
-        'required': False,
-        'default': None,
-        'extendable': Extendable.NO,
-        'indexable': False,
-    },
-    'REVISION': {
-        'type': types.IntType,
-        'required': True,
-        'default': 1,
-        'extendable': Extendable.NO,
-        'indexable': False,
-    },
+	# non-extendable and non-indexable, i.e. per-port attributes
+	'BUILD_PACKAGE_ACTIVATION_PHASE': {
+		'type': Phase,
+		'required': False,
+		'default': Phase('BUILD'),
+		'extendable': Extendable.NO,
+		'indexable': False,
+	},
+	'DISABLE_SOURCE_PACKAGE': {
+		'type': YesNo,
+		'required': False,
+		'default': False,
+		'extendable': Extendable.NO,
+		'indexable': False,
+	},
+	'HOMEPAGE': {
+		'type': types.ListType,
+		'required': True,
+		'default': None,
+		'extendable': Extendable.NO,
+		'indexable': False,
+	},
+	'MESSAGE': {
+		'type': types.StringType,
+		'required': False,
+		'default': None,
+		'extendable': Extendable.NO,
+		'indexable': False,
+	},
+	'REVISION': {
+		'type': types.IntType,
+		'required': True,
+		'default': 1,
+		'extendable': Extendable.NO,
+		'indexable': False,
+	},
 
-    # indexable, i.e. per-source attributes
-    'ADDITIONAL_FILES': {
-        'type': types.ListType,
-        'required': False,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
-    'CHECKSUM_SHA256': {
-        'type': types.StringType,
-        'required': False,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
-    'PATCHES': {
-        'type': types.ListType,
-        'required': False,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
-    'SOURCE_DIR': {
-        'type': types.StringType,
-        'required': False,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
-    'SRC_FILENAME': {
-        'type': types.StringType,
-        'required': False,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
-    'SRC_URI': {
-        'type': types.ListType,
-        'required': True,
-        'default': {},
-        'extendable': Extendable.NO,
-        'indexable': True,
-    },
+	# indexable, i.e. per-source attributes
+	'ADDITIONAL_FILES': {
+		'type': types.ListType,
+		'required': False,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
+	'CHECKSUM_SHA256': {
+		'type': types.StringType,
+		'required': False,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
+	'PATCHES': {
+		'type': types.ListType,
+		'required': False,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
+	'SOURCE_DIR': {
+		'type': types.StringType,
+		'required': False,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
+	'SRC_FILENAME': {
+		'type': types.StringType,
+		'required': False,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
+	'SRC_URI': {
+		'type': types.ListType,
+		'required': True,
+		'default': {},
+		'extendable': Extendable.NO,
+		'indexable': True,
+	},
 
-    # extendable, i.e. per-package attributes
-    'ARCHITECTURES': {
-        'type': Architectures,
-        'required': True,
-        'default': None,
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-    },
-    'BUILD_PREREQUIRES': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'BUILD_REQUIRES': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'CONFLICTS': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'COPYRIGHT': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-    },
-    'DESCRIPTION': {
-        'type': LinesOfText,
-        'required': True,
-        'default': None,
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-    },
-    'FRESHENS': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'GLOBAL_WRITABLE_FILES': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'LICENSE': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-    },
-    'PACKAGE_GROUPS': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'PACKAGE_NAME': {
-        'type': types.StringType,
-        'required': False,
-        'default': None,
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'PACKAGE_USERS': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'PACKAGE_VERSION': {
-        'type': types.StringType,
-        'required': False,
-        'default': None,
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'PROVIDES': {
-        'type': ProvidesList,
-        'required': True,
-        'default': None,
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'POST_INSTALL_SCRIPTS': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'REPLACES': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'REQUIRES': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'SECONDARY_ARCHITECTURES': {
-        'type': Architectures,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-    },
-    'SUMMARY': {
-        'type': types.StringType,
-        'required': True,
-        'default': None,
-        'extendable': Extendable.INHERITED,
-        'indexable': False,
-        'suffix': {
-            PackageType.DEVELOPMENT: ' (development files)',
-            PackageType.DEBUG_INFO: ' (debug info)',
-            PackageType.DOCUMENTATION: ' (documentation)',
-            PackageType.SOURCE: ' (source files)',
-        }
-    },
-    'SUPPLEMENTS': {
-        'type': RequiresList,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
-    'USER_SETTINGS_FILES': {
-        'type': types.ListType,
-        'required': False,
-        'default': [],
-        'extendable': Extendable.DEFAULT,
-        'indexable': False,
-    },
+	# extendable, i.e. per-package attributes
+	'ARCHITECTURES': {
+		'type': Architectures,
+		'required': True,
+		'default': None,
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+	},
+	'BUILD_PREREQUIRES': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'BUILD_REQUIRES': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'CONFLICTS': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'COPYRIGHT': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+	},
+	'DESCRIPTION': {
+		'type': LinesOfText,
+		'required': True,
+		'default': None,
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+	},
+	'FRESHENS': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'GLOBAL_WRITABLE_FILES': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'LICENSE': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+	},
+	'PACKAGE_GROUPS': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'PACKAGE_NAME': {
+		'type': types.StringType,
+		'required': False,
+		'default': None,
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'PACKAGE_USERS': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'PACKAGE_VERSION': {
+		'type': types.StringType,
+		'required': False,
+		'default': None,
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'PROVIDES': {
+		'type': ProvidesList,
+		'required': True,
+		'default': None,
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'POST_INSTALL_SCRIPTS': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'REPLACES': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'REQUIRES': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'SECONDARY_ARCHITECTURES': {
+		'type': Architectures,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+	},
+	'SUMMARY': {
+		'type': types.StringType,
+		'required': True,
+		'default': None,
+		'extendable': Extendable.INHERITED,
+		'indexable': False,
+		'suffix': {
+			PackageType.DEVELOPMENT: ' (development files)',
+			PackageType.DEBUG_INFO: ' (debug info)',
+			PackageType.DOCUMENTATION: ' (documentation)',
+			PackageType.SOURCE: ' (source files)',
+		}
+	},
+	'SUPPLEMENTS': {
+		'type': RequiresList,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
+	'USER_SETTINGS_FILES': {
+		'type': types.ListType,
+		'required': False,
+		'default': [],
+		'extendable': Extendable.DEFAULT,
+		'indexable': False,
+	},
 }
 
 def getRecipeAttributes():
-    return copy.deepcopy(recipeAttributes)
+	return copy.deepcopy(recipeAttributes)
