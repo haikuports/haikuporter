4a148f26d9aded2864f096a54f04e9faf5b7a780
Oliver Tappe <zooey@hirschkaefer.de>
2014-11-20 23:40:14 +0100

    Add new DependencyResolver.
    * Instead of delegating the dependency resolution to pkgman, this class
      implements a manual, graph-based dependency resolution algorithm:
      + Starting from a set of given dependency info files, a graph of
        package nodes is built, taking into account the different types
        of requires (requires, build-requires and build-prerequires).
      + For each package node, all the dependencies are processed and
        (depending on the type of file that the package node represents)
        added to the graph, too, until all dependencies have been handled.
      + The resulting graph is sorted topologically, yielding a list of
        packages that can be built one after the other in that order.

---
+++ HaikuPorter/DependencyResolver.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Ingo Weinhold
+# Distributed under the terms of the MIT License.
+
+# -- Modules ------------------------------------------------------------------
+
+import os
+from subprocess import CalledProcessError
+
+from .Options import getOption
+from .PackageInfo import PackageInfo
+from .ProvidesManager import ProvidesManager
+from .Utils import printError, sysExit
+
+# -- PackageNode class --------------------------------------------------------
+
+class PackageNode(object):
+	def __init__(self, packageInfo, isBuildhostPackage):
+		self.packageInfo = packageInfo
+		self.realPath = os.path.realpath(packageInfo.path)
+		self.isBuildhostPackage = isBuildhostPackage
+		self.dependencyCount = 0
+
+	def __eq__(self, other):
+		return (self.packageInfo.name == other.packageInfo.name
+			and self.packageInfo.version == other.packageInfo.version
+			and self.realPath == other.realPath
+			and self.isBuildhostPackage == other.isBuildhostPackage)
+
+	def __str__(self):
+		return '%s-%s :: %s ::%s' % (self.packageInfo.name,
+									 self.packageInfo.version,
+									 self.realPath,
+									 self.isBuildhostPackage)
+
+	@property
+	def versionedName(self):
+		return self.packageInfo.versionedName
+
+	@property
+	def path(self):
+		return self.packageInfo.path
+
+	def bumpDependencyCount(self):
+		self.dependencyCount += 1
+
+# -- DependencyResolver class ----------------------------------------------------
+
+class DependencyResolver(object):
+	def __init__(self, buildPlatform, requiresTypes, repositories, **kwargs):
+		self._providesManager = ProvidesManager()
+		self._platform = buildPlatform
+		self._requiresTypes = requiresTypes
+		self._repositories = repositories
+		self._stopAtHpkgs = kwargs.get('stopAtHpkgs', False)
+		self._packageNodes = []
+
+	def determineRequiredPackagesFor(self, dependencyInfoFiles):
+		for repository in self._repositories:
+			for entry in os.listdir(repository):
+				if not (entry.endswith('.DependencyInfo')
+						or entry.endswith('.hpkg')
+						or entry.endswith('.PackageInfo')):
+					continue
+				packageInfo = self._parsePackageInfo(repository + '/' + entry)
+				self._providesManager.addProvidesFromPackageInfo(packageInfo)
+
+		packageInfos = [
+			self._parsePackageInfo(dif) for dif in dependencyInfoFiles
+		]
+		self._pending = [
+			PackageNode(pi, False) for pi in packageInfos
+		]
+		self._traversed = set([
+			str(PackageNode(pi, False)) for pi in packageInfos
+		])
+
+		self._buildDependencyGraph()
+
+		self._sortPackageNodesTopologically()
+
+		result = [
+			node.path for node in self._packageNodes
+		]
+
+		return result
+
+	def _buildDependencyGraph(self):
+		numberOfInitialPackages = len(self._pending)
+		numberOfHandledPackages = 0
+		while self._pending:
+			packageNode = self._pending.pop(0)
+
+			if 'REQUIRES' in self._requiresTypes:
+				self._addAllImmediateRequiresOf(packageNode)
+			if 'BUILD_REQUIRES' in self._requiresTypes:
+				self._addAllImmediateBuildRequiresOf(packageNode)
+			if 'BUILD_PREREQUIRES' in self._requiresTypes:
+				self._addAllImmediateBuildPrerequiresOf(packageNode)
+
+			# when the batch of passed in packages has been handled, we need
+			# to activate the REQUIRES, too, since these are needed to run
+			# all the following packages
+			numberOfHandledPackages += 1
+			if (numberOfHandledPackages == numberOfInitialPackages):
+				self._requiresTypes.append('REQUIRES')
+
+	def _sortPackageNodesTopologically(self):
+		sortedPackageNodes = []
+		while self._packageNodes:
+			lowestDependencyCount = 1000000
+			nodesWithLowestDependencyCount = []
+			for node in self._packageNodes:
+				if lowestDependencyCount > node.dependencyCount:
+					lowestDependencyCount = node.dependencyCount
+					nodesWithLowestDependencyCount = [node]
+				elif lowestDependencyCount == node.dependencyCount:
+					nodesWithLowestDependencyCount.append(node)
+
+			sortedPackageNodes += nodesWithLowestDependencyCount
+			self._packageNodes = [
+				node for node in self._packageNodes
+				if node not in nodesWithLowestDependencyCount
+			]
+
+		self._packageNodes = sortedPackageNodes
+
+	def _addAllImmediateRequiresOf(self, requiredPackageInfo):
+		packageInfo = requiredPackageInfo.packageInfo
+		forBuildhost = requiredPackageInfo.isBuildhostPackage
+
+		for requires in packageInfo.requires:
+			self._addImmediate(requiredPackageInfo, requires, 'requires',
+							   forBuildhost)
+
+	def _addAllImmediateBuildRequiresOf(self, requiredPackageInfo):
+		packageInfo = requiredPackageInfo.packageInfo
+		forBuildhost = requiredPackageInfo.isBuildhostPackage
+
+		for requires in packageInfo.buildRequires:
+			self._addImmediate(requiredPackageInfo, requires, 'build-requires',
+							   forBuildhost)
+
+	def _addAllImmediateBuildPrerequiresOf(self, requiredPackageInfo):
+		packageInfo = requiredPackageInfo.packageInfo
+
+		for requires in packageInfo.buildPrerequires:
+			self._addImmediate(requiredPackageInfo, requires,
+							   'build-prerequires', True)
+
+	def _addImmediate(self, parent, requires, typeString, forBuildhost):
+		implicitProvides = self._platform.getImplicitProvides(forBuildhost)
+		isImplicit = requires.name in implicitProvides
+		# Skip, if this is one of the implicit provides of the build platform,
+		# unless we are collecting the source packages for the bootstrap, in
+		# case of which we try to add all requires (as in that case the actual
+		# buildhost is Haiku and we need to put the corresponding source
+		# packages onto the bootstrap image).
+		if isImplicit and not getOption('createSourcePackagesForBootstrap'):
+			return
+
+		provides = self._providesManager.getMatchingProvides(requires)
+		if not provides:
+			if isImplicit:
+				return
+			printError('%s "%s" of package "%s" could not be resolved'
+					   % (typeString, str(requires), parent.versionedName))
+			raise LookupError()
+
+		requiredPackageInfo = PackageNode(provides.packageInfo, forBuildhost)
+		if requiredPackageInfo.path.endswith('.hpkg'):
+			self._addPackageNode(requiredPackageInfo, not self._stopAtHpkgs)
+		else:
+			parent.bumpDependencyCount()
+			self._addPackageNode(requiredPackageInfo, True)
+
+	def _addPackageNode(self, requiredPackageInfo, addToPending):
+		if str(requiredPackageInfo) not in self._traversed:
+			self._traversed.add(str(requiredPackageInfo))
+			self._packageNodes.append(requiredPackageInfo)
+			if addToPending:
+				self._pending.append(requiredPackageInfo)
+
+	def _parsePackageInfo(self, packageInfoFile):
+		try:
+			packageInfo = PackageInfo(packageInfoFile)
+		except CalledProcessError:
+			sysExit('failed to parse "%s"' % packageInfoFile)
+
+		return packageInfo
