haikuports/haikuporter

commit 210c9e5ebe4784b2000b51bd0792ad45808365b4

RequiresUpdater: Rework to use ProvidesManager.
As mentioned in 0e0360dbb88813a30a9f8f81b9a5093fbaf303ee the
ProvidesManager was extracted from the RequiresUpdater with the
intention of making it use the ProvidesManager. This commit does so.

HaikuPorter/RequiresUpdater.py
@@ -22,13 +22,11 @@ class ProvidesInfo(Resolvable):
 
 class RequiresUpdater(object):
 	def __init__(self, portPackages, requiredPackages):
-		self.providesMap = {}
+		self._providesManager = ProvidesManager()
 
 		# get the provides for the port packages
 		for package in portPackages:
-			for providesString in package.recipeKeys['PROVIDES']:
-				self._addPackageProvidesInfo(package.revisionedName,
-					providesString)
+			self._providesManager.addProvidesFromPackage(package)
 
 		# get the provides for the required packages
 		for package in requiredPackages:
@@ -41,8 +39,7 @@ class RequiresUpdater(object):
 		except CalledProcessError:
 			sysExit('failed to get provides for package "%s"' % package)
 
-		for provides in packageInfo.provides:
-			self._addPackageProvidesInfo(package, str(provides))
+		self._providesManager.addProvidesFromPackageInfo(packageInfo)
 
 	def addPackages(self, directory):
 		for package in os.listdir(directory):
@@ -58,35 +55,7 @@ class RequiresUpdater(object):
 		return result
 
 	def getMatchingProvides(self, resolvableExpression):
-		name = resolvableExpression.name
-		operator = resolvableExpression.operator
-		version = resolvableExpression.version
-
-		if not name in self.providesMap:
-			return None
-
-		providesList = self.providesMap[name]
-		for provides in providesList:
-			if not operator:
-				return provides
-			if not provides.version:
-				continue
-			matches = {
-				'<':	lambda compareResult: compareResult < 0,
-				'<=':	lambda compareResult: compareResult <= 0,
-				'==':	lambda compareResult: compareResult == 0,
-				'!=':	lambda compareResult: compareResult != 0,
-				'>=':	lambda compareResult: compareResult >= 0,
-				'>':	lambda compareResult: compareResult > 0,
-			}[operator](versionCompare(provides.version, version))
-			if not matches:
-				continue
-			if (provides.compatibleVersion
-				and versionCompare(provides.compatibleVersion, version)
-					> 0):
-				continue
-			return provides
-		return None
+		self._providesManager.getMatchingProvides(resolvableExpression)
 
 	def _updateRequires(self, requires):
 		# split the requires string
@@ -121,10 +90,3 @@ class RequiresUpdater(object):
 		if isBase:
 			result += ' base'
 		return result
-
-	def _addPackageProvidesInfo(self, package, providesString):
-		provides = ProvidesInfo(package, providesString.strip())
-		if provides.name in self.providesMap:
-			self.providesMap[provides.name].append(provides)
-		else:
-			self.providesMap[provides.name] = [ provides ]

HaikuPorter/ProvidesManager.py
@@ -30,6 +30,10 @@ class ProvidesManager(object):
 	def __init__(self):
 		self._providesMap = {}
 
+	def addProvidesFromPackage(self, package):
+		for providesString in package.recipeKeys['PROVIDES']:
+			self._addPackageProvidesInfo(package.revisionedName, providesString)
+
 	def addProvidesFromPackageInfo(self, packageInfo):
 		for provides in packageInfo.provides:
 			self._addPackageProvidesInfo(packageInfo, str(provides))

commit 13b41e85f1829d7938eeb7e331b4f435ae8c7023

RequiresUpdater: Fix bug introduced in 210c9e5e.
* fix bug #83. All hpkgs produced since then are broken.

HaikuPorter/RequiresUpdater.py
@@ -56,7 +56,7 @@ class RequiresUpdater(object):
 		return result
 
 	def getMatchingProvides(self, resolvableExpression):
-		self._providesManager.getMatchingProvides(resolvableExpression)
+		return self._providesManager.getMatchingProvides(resolvableExpression)
 
 	def _updateRequires(self, requires):
 		# split the requires string