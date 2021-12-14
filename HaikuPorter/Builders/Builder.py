f0840deddf5abfafcc15e40b25c9ad138b35d957
Alexander von Gluck IV <kallisti5@unixzen.com>
2021-12-14 12:11:17 -0600

    BuildMaster: Cleanup builders. Untangle spaghetti

---
+++ HaikuPorter/Builders/Builder.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Michael Lotz
+# Copyright 2016 Jerome Duval
+# Distributed under the terms of the MIT License.
+
+class _BuilderState(object):
+	AVAILABLE = 'Available'
+	LOST = 'Lost'
+	NOT_AVAILABLE = 'Not Available'
+	RECONNECT = 'Reconnecting'
