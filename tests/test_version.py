31fee8187f9ad56581a9e50fef102e8a2a0ae7f8
jurgenwigg <jurgenwigg@protonmail.com>
2023-07-18 13:38:15 +0000

    working PoC

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,4 +4,5 @@ from pytest import mark
 
 @mark.parametrize("input_ver", ["1", "1.1", "1.1.1", "1.1.1~1", "1.1.1-1"])
 def test_basic(input_ver):
-    result = version.HpkgVersion(input_ver)
+    assert version.HpkgVersion(input_ver)
+
