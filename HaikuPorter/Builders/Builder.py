af1d1ddc87651ab751af55f5ea114fab67da3ac8
jurgenwigg <jurgenwigg@protonmail.com>
2023-09-28 12:03:29 +0000

    refactor: changed Builder.py

--- HaikuPorter/Builders/Builder.py
+++ HaikuPorter/Builders/Builder.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-#
 # Copyright 2015 Michael Lotz
 # Copyright 2016 Jerome Duval
 # Distributed under the terms of the MIT License.
+from enum import Enum
 
-class _BuilderState(object):
-	AVAILABLE = 'Available'
-	LOST = 'Lost'
-	NOT_AVAILABLE = 'Not Available'
-	RECONNECT = 'Reconnecting'
+
+class BuilderState(Enum):
+    AVAILABLE = "Available"
+    LOST = "Lost"
+    NOT_AVAILABLE = "Not Available"
+    RECONNECT = "Reconnecting"
