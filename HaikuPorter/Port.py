haikuports/haikuporter

commit e1d6715aea62d5b5c0e00401a863ee197eec2258

fix "any" arch subpackages on secondary arch
When determining the architecture of a package, check whether it is "any"
before checking the secondary architecture.

To make "any" packages identical to the case when not using a secondary
architecture, don't include the secondaryArchSuffix in the package name in this
case. This is a breaking change! Any packages using this must update the
PROVIDES of the subpackage and should add a REPLACES entry to facilitate
upgrades.

Add support for this change to ShellScriptlets getPackagePrefix, which is also
used by packageEntries.

Fixes #320.

HaikuPorter/Port.py
@@ -29,7 +29,7 @@ from .Configuration import Configuration
 from .Options import getOption
 from .Package import PackageType, packageFactory, sourcePackageFactory
 from .RecipeAttributes import getRecipeAttributes
-from .RecipeTypes import Extendable, MachineArchitecture, Phase, Status
+from .RecipeTypes import Architectures, Extendable, MachineArchitecture, Phase, Status
 from .ReleaseChecker import createReleaseChecker
 from .RequiresUpdater import RequiresUpdater
 from .ShellScriptlets import (cleanupChrootScript, getShellVariableSetters,
@@ -1000,10 +1000,12 @@ class Port(object):
 		haveSourcePackage = False
 		for extension in sorted(self.recipeKeysByExtension.keys()):
 			keys = self.recipeKeysByExtension[extension]
-			if extension:
-				name = self.name + '_' + extension
+			if Architectures.ANY in keys['ARCHITECTURES']:
+				name = self.baseName
 			else:
 				name = self.name
+			if extension:
+				name += '_' + extension
 			packageType = PackageType.byName(extension)
 			package = packageFactory(packageType, name, self, keys, self.policy)
 			self.allPackages.append(package)

HaikuPorter/Package.py
@@ -96,6 +96,8 @@ class Package(object):
 				self.architecture = port.targetArchitecture
 			else:
 				self.architecture = Architectures.SOURCE
+		elif Architectures.ANY in self.recipeKeys['ARCHITECTURES']:
+			self.architecture = Architectures.ANY
 		elif ((port.secondaryArchitecture is not None and
 			  port.secondaryArchitecture in self.recipeKeys['SECONDARY_ARCHITECTURES']) or
 			  port.targetArchitecture in self.recipeKeys['ARCHITECTURES']):
@@ -104,8 +106,6 @@ class Package(object):
 			# is the same as the target architecture, except for "_cross_"
 			# packages, which are built for the host on which the build runs.)
 			self.architecture = port.hostArchitecture
-		elif Architectures.ANY in self.recipeKeys['ARCHITECTURES']:
-			self.architecture = Architectures.ANY
 		else:
 			sysExit('package %s cannot be built for architecture %s'
 					% (self.versionedName, port.targetArchitecture))
@@ -142,8 +142,9 @@ class Package(object):
 
 	def getStatusOnSecondaryArchitecture(self, architecture,
 			secondaryArchitecture):
-		# check the secondary architecture
-		if secondaryArchitecture:
+		# check the secondary architecture (but ANY takes precedence)
+		if (secondaryArchitecture
+			and not Architectures.ANY in self.recipeKeys['ARCHITECTURES']):
 			secondaryStatus = Status.UNSUPPORTED
 			secondaryArchitectures = self.recipeKeys['SECONDARY_ARCHITECTURES']
 			if secondaryArchitecture in secondaryArchitectures:

HaikuPorter/ShellScriptlets.py
@@ -91,8 +91,14 @@ getPackagePrefix()
 	local linksDir="$packageLinksDir/$packageName-$packageVersion-$REVISION"
 	local packagePrefix="$linksDir/.self"
 	if [ ! -e "$packagePrefix" ]; then
-		echo >&2 "packageEntries: warning: \"$packageSuffix\" doesn't seem to be a valid package suffix."
-		exit 1
+		# try again with the base name
+		local packageName="${portName/%$secondaryArchSuffix}_$packageSuffix"
+		local linksDir="$packageLinksDir/$packageName-$packageVersion-$REVISION"
+		local packagePrefix="$linksDir/.self"
+		if [ ! -e "$packagePrefix" ]; then
+			echo >&2 "packageEntries: warning: \"$packageSuffix\" doesn't seem to be a valid package suffix."
+			exit 1
+		fi
 	fi
 
 	echo $packagePrefix

commit f2c17938b1a2bbf4e0edc28f53e9d75c70cd7fcc

fix "any" arch ports being considered buildable on secondaryArch
This fixes a regression from e1d6715aea62d5b5c0e00401a863ee197eec2258, which
erroneously considered 'any' arch ports (where the main package is "any") as
buildable on secondary architectures. Besides that this doesn't make sense
logically, in practice these packages are never buildable because the value of
$prefix is wrong in this case because the name of the main package doesn't
match the port name (and $prefix is used by all recipes directly or indirectly
because it is the target for all files to be installed to). The above commit
only added support for getPackagePrefix for the subpackages, not the main one.

Additionally, this bug caused the package-to-port associations in the
repository to become wrong and always point to the (last) secondaryArch port
instead of the primary one because secondary architectures are processed after
the primary architecture.

This change fixes the bug by marking ports with an "any" arch main package as
explicitly unsupported. This can't be done at the package level, because it
doesn't know whether it is the main package or a subpackage.

HaikuPorter/Port.py
@@ -470,6 +470,12 @@ class Port(object):
 			self.parseRecipeFileIfNeeded()
 
 			# use the status of the base package as overall status of the port
+			if self.allPackages[0].architecture == Architectures.ANY and self.secondaryArchitecture:
+				# if the base package is 'any', the secondary architecture is not supported
+				# TODO: architecture-specific subpackages are not considered, so if a port uses
+				#       secondary architecture for them, it will not work.
+                #       We should check all packages instead, not just the base package.
+				return Status.UNSUPPORTED
 			return self.allPackages[0].getStatusOnSecondaryArchitecture(
 				self.targetArchitecture, self.secondaryArchitecture)
 		except: