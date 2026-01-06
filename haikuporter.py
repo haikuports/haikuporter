7ed5ece5140747b0485f61e85b10d68d03385e31
Joachim Mairböck <j.mairboeck@gmail.com>
2026-01-06 21:43:24 +0100

    switch build system to flit-core, clean up dependencies
     * Remove codecov and pytest-cov
     * Remove tox.ini
     * Cleanup .gitignore (mostly restore the version before #259)
    
    Co-authored-by: Oscar Lesta <oscar.lesta@gmail.com>

--- haikuporter.py
+++ haikuporter.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013-2014 Oliver Tappe
+# Copyright 2026 HaikuPorter contributors
 # Distributed under the terms of the MIT License.
-import logging
+from HaikuPorter.__main__ import main
 
-from HaikuPorter.Main import Main
-from HaikuPorter.Options import parseOptions
-
-logger = logging.getLogger("buildLogger")
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-
-Main(*parseOptions())
+main()
